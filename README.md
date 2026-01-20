435f312be953be884079e14f123ea80a47283cd5
ParkSoHyeon <dso753@naver.com>
2026-01-20 16:53:19 +0900

    init commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# 한입 로그
+
+---
+
+## 기술 스택 (Tech Stack)
+
+### Frontend
+- **React**
+- **TypeScript**
+- **Vite**
+- **Tailwind CSS**  
+  유틸리티 기반 CSS 프레임워크
+- **Zustand**  
+  전역 상태 관리 라이브러리
+- **TanStack React Query**  
+  서버 상태 관리 및 데이터 패칭
+- **shadcn/ui**  
+  Tailwind 기반의 재사용 가능한 UI 컴포넌트
+
+### Mock Server
+- **json-server**  
+  JSON 파일 기반의 Mock REST API 서버
+
+### Infrastructure
+- **Docker**
+- **Docker Compose**
+
+---
+
+## 1. Docker 컨테이너 실행
+
+```bash
+docker compose up -d sns-app
+```
+
+## 2. json-server로 Mock API 서버 실행
+
+```bash
+# 실행 중인 컨테이너 내부 접속
+docker exec -it sns-app sh
+
+# mock api 서버 실행
+npx json-server server/db.json --host 0.0.0.0 --port 5200
+```
+
---
+++ src/main.tsx
@@ -0,0 +1,30 @@
+import { createRoot } from "react-dom/client"
+import "./index.css"
+import App from "./App.tsx"
+import { BrowserRouter } from "react-router";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+// 서버 상태 전용 저장소
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+      gcTime: 5 * 60 * 1000, // 5분
+
+      refetchOnMount: true,
+      refetchOnReconnect: false,
+      refetchOnWindowFocus: false,
+      refetchInterval: false
+    },
+  },
+});
+
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      <App />
+    </QueryClientProvider>
+  </BrowserRouter>,
+);
