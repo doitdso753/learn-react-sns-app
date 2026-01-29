4c8559db03ae9fc574327e78b8c6db255f7d966b
ParkSoHyeon <dso753@naver.com>
2026-01-29 18:54:26 +0900

    fix: 캐시 최적화 - 포스트 삭제 시, 리스트 데이터 다시 불러오기

--- src/hooks/mutations/post/use-delete-post.ts
+++ src/hooks/mutations/post/use-delete-post.ts
@@ -1,9 +1,12 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deletePost } from "@/api/post.ts";
 import type { UseMutationCallback } from "@/types.ts";
 import { deleteImageInPath } from "@/api/image.ts";
+import { QUERY_KEYS } from "@/lib/constants.ts";
 
 export function useDeletePost(callbacks: UseMutationCallback) {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: deletePost,
     onSuccess: async (deletedPost) => {
@@ -14,6 +17,10 @@ export function useDeletePost(callbacks: UseMutationCallback) {
       if ((deletedPost.image_urls ?? []).length > 0) {
         await deleteImageInPath(`${deletedPost.author_id}/${deletedPost.id}`);
       }
+
+      queryClient.resetQueries({
+        queryKey: QUERY_KEYS.post.list,
+      });
     },
     onError: (error) => {
       console.error(error);
