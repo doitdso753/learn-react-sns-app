bf9269e56a6f11392c88eede79aa29fa183f55df
ParkSoHyeon <dso753@naver.com>
2026-06-29 13:03:12 +0900

    feat: 댓글 기능 추가 - 댓글 등록, 수정, 삭제 시 캐시 데이터 갱신되도록 수정 - 댓글 조회 오래된순으로 정렬되도록 수정

--- src/hooks/mutations/comment/use-delete-comment.ts
+++ src/hooks/mutations/comment/use-delete-comment.ts
@@ -1,14 +1,24 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteComment } from "@/api/comment.ts";
-import type { UseMutationCallback } from "@/types.ts";
+import type { Comment, UseMutationCallback } from "@/types.ts";
+import { QUERY_KEYS } from "@/lib/constants.ts";
 
 export default function useDeleteComment(callback?: UseMutationCallback) {
+  const queryClient = useQueryClient();
   return useMutation({
     mutationFn: deleteComment,
-    onSuccess: () => {
+    onSuccess: (deletedComment) => {
       if (callback?.onSuccess) {
         callback.onSuccess();
       }
+
+      queryClient.setQueryData<Comment[]>(
+        QUERY_KEYS.comment.post(deletedComment.post_id),
+        (comments) => {
+          if (!comments) throw new Error("댓글이 캐시 데이터에 보관되어있지 않습니다.");
+          return comments?.filter((comment) => comment.id !== deletedComment.id);
+        },
+      );
     },
     onError: error => {
       console.error(error);
